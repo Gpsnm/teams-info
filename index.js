79f794d920f50e400c07ed876b50e4abea0754d9
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-04 11:53:36 +0000

    added comments to my code

--- index.js
+++ index.js
@@ -56,124 +56,124 @@ function start() {
         createTeam();
       });
   }
-
-function createEngineer() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "engineerName",
-        message: "What is the Engineers name? ",
-        validate: (answer) => {
-          if (answer != "") {
-            return true;
-          }
-          return "Please enter at least one character";
+// function to create the engineer
+  function createEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is the Engineers name? ",
+          validate: (answer) => {
+            if (answer != "") {
+              return true;
+            }
+            return "Please enter at least one character";
+          },
         },
-      },
-      {
-        type: "input",
-        name: "engineerId",
-        message: "What is the engineers employee id number?",
-      },
-      {
-        type: "input",
-        name: "engineerEmail",
-        message: "What is the engineers email address ?",
-      },
-      {
-        type: "input",
-        name: "engineerGitHub",
-        message: "What is the engineers Github username? ",
-      },
-    ])
-    .then((answers) => {
-      const engineer = new Engineer(
-        answers.engineerName,
-        answers.engineerId,
-        answers.engineerEmail,
-        answers.engineerGitHub
-      );
-      team.push(engineer);
-      createTeam();
-    });
-}
-
-function createIntern() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "internName",
-        message: "What is the Interns name? ",
-        validate: (answer) => {
-          if (answer != "") {
-            return true;
-          }
-          return "Please enter at least one character";
+        {
+          type: "input",
+          name: "engineerId",
+          message: "What is the engineers employee id number?",
         },
-      },
-      {
-        type: "input",
-        name: "internId",
-        message: "What is the interns employee id number?",
-      },
-      {
-        type: "input",
-        name: "internEmail",
-        message: "What is the interns email address ?",
-      },
-      {
-        type: "input",
-        name: "internSchool",
-        message: "What school does the intern attend?",
-      },
-    ])
-    .then((answers) => {
-      const intern = new Intern(
-        answers.internName,
-        answers.internId,
-        answers.internEmail,
-        answers.internSchool
-      );
-      team.push(intern);
-      createTeam();
-    });
-}
-
-function createTeam() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "teamChoice",
-        message: "what type of team member would you like to add?",
-        choices: ["Engineer", "Intern", "I don't want to add anything else"],
-      },
-    ])
-    .then((answers) => {
-      switch (answers.teamChoice) {
-        case "Engineer":
-          createEngineer();
-          break;
-        case "Intern":
-          createIntern();
-          break;
-        default:
-          buildTeam();
-      }
-    });
-}
-
-
-function buildTeam(){
-  if (!fs.existsSync(OUTPUT_DIR)) {
-      fs.mkdirSync(OUTPUT_DIR);}
+        {
+          type: "input",
+          name: "engineerEmail",
+          message: "What is the engineers email address ?",
+        },
+        {
+          type: "input",
+          name: "engineerGitHub",
+          message: "What is the engineers Github username? ",
+        },
+      ])
+      .then((answers) => {
+        const engineer = new Engineer(
+          answers.engineerName,
+          answers.engineerId,
+          answers.engineerEmail,
+          answers.engineerGitHub
+        );
+        team.push(engineer);
+        createTeam();
+      });
+  }
+// function to create the intern
+  function createIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "What is the Interns name? ",
+          validate: (answer) => {
+            if (answer != "") {
+              return true;
+            }
+            return "Please enter at least one character";
+          },
+        },
+        {
+          type: "input",
+          name: "internId",
+          message: "What is the interns employee id number?",
+        },
+        {
+          type: "input",
+          name: "internEmail",
+          message: "What is the interns email address ?",
+        },
+        {
+          type: "input",
+          name: "internSchool",
+          message: "What school does the intern attend?",
+        },
+      ])
+      .then((answers) => {
+        const intern = new Intern(
+          answers.internName,
+          answers.internId,
+          answers.internEmail,
+          answers.internSchool
+        );
+        team.push(intern);
+        createTeam();
+      });
+  }
+// create team function that is called after every employee created and creates a new employee based on users input.
+  function createTeam() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "teamChoice",
+          message: "what type of team member would you like to add?",
+          choices: ["Engineer", "Intern", "I don't want to add anything else"],
+        },
+      ])
+      .then((answers) => {
+        switch (answers.teamChoice) {
+          case "Engineer":
+            createEngineer();
+            break;
+          case "Intern":
+            createIntern();
+            break;
+          default:
+            buildTeam();
+        }
+      });
+  }
+// function that populates HTML document with relevant information.
+  function buildTeam() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+    }
     fs.writeFileSync(outputPath, render(team));
     console.log("successfully generated team profile!");
   }
 
-createManager()
-};
-
+  createManager();
+}
+// starts the whole function off.
 start();
