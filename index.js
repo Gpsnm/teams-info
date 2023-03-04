7f9666bb390ded1dd76dfc377d115e8eb0a6804b
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-04 10:42:18 +0000

    created getIntern function

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ function start() {
               return true;
             }
             return "Please enter at least one character";
-          }
+          },
         },
         {
-            type:"input",
-            name:"managerId",
-            message:"What is the managers employee id number?"
-          },
+          type: "input",
+          name: "managerId",
+          message: "What is the managers employee id number?",
+        },
         {
           type: "input",
           name: "managerEmail",
@@ -58,57 +58,97 @@ function start() {
   }
 }
 
-
-function createTeam(){
+function createTeam() {
   inquirer.prompt([
     {
-      type:"list",
-      name:"teamChoice",
-      message:"what type of team member would you like to add?",
-      choices:["Engineer","Intern","I don't want to add anything else"],
-    }
-  ]
-  )
+      type: "list",
+      name: "teamChoice",
+      message: "what type of team member would you like to add?",
+      choices: ["Engineer", "Intern", "I don't want to add anything else"],
+    },
+  ]);
 }
 
-function createEngineer(){
+function createEngineer() {
   inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "engineerName",
-          message: "What is the Engineers name? ",
-          validate: (answer) => {
-            if (answer != "") {
-              return true;
-            }
-            return "Please enter at least one character";
-          },
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is the Engineers name? ",
+        validate: (answer) => {
+          if (answer != "") {
+            return true;
+          }
+          return "Please enter at least one character";
         },
-        {
-          type:"input",
-          name:"engineerId",
-          message:"What is the engineers employee id number?"
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is the engineers employee id number?",
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineers email address ?",
+      },
+      {
+        type: "input",
+        name: "engineerGitHub",
+        message: "What is the engineers Github username? ",
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.engineer,
+        answers.engineerId,
+        answers.engineerEmail,
+        answers.engineerGithub
+      );
+      team.push(engineer);
+      createTeam();
+    });
+}
+
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "InternName",
+        message: "What is the Interns name? ",
+        validate: (answer) => {
+          if (answer != "") {
+            return true;
+          }
+          return "Please enter at least one character";
         },
-        {
-          type: "input",
-          name: "engineerEmail",
-          message: "What is the engineers email address ?",
-        },
-        {
-          type: "input",
-          name: "engineerGitHub",
-          message: "What is the engineers Github username? ",
-        },
-      ])
-      .then((answers) => {
-        const engineer = new Engineer(
-          answers.engineer,
-          answers.engineerId,
-          answers.engineerEmail,
-          answers.engineerGithub
-        );
-        team.push(engineer);
-        // call the next function that will ask what type of employee will be created next
-        createTeam();
-})}
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "What is the engineers employee id number?",
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is the engineers email address ?",
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What school does the inturn attend?",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.intern,
+        answers.internId,
+        answers.internEmail,
+        answers.internSchool
+      );
+      team.push(intern);
+      createTeam();
+    });
+}
